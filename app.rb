d2d350d163d65e52cc043643bf421c07678ba1cd
luigineto <luigineto@hotmail.com>
2017-01-08 20:28:33 +0100

    add Artist variables at artist's page

--- app.rb
+++ app.rb
@@ -6,5 +6,21 @@ get('/') do
 end
 
 get('/linocutboy') do
+	@art_title_01 = "Handbag"
+	@art_image_01 = "/img/linocutboy_01_handbag.jpeg"
+	@art_description_01 = "Linocut and screenprint | 76x100 cm"
+
+	@art_title_02 = "Deep Sea Diver"
+	@art_image_02 = "/img/linocutboy_02_deep_sea_diver.jpeg"
+	@art_description_02 = "Linocut | 76x45 cm"
+
+	@art_title_03 = "Blue Turkeys"
+	@art_image_03 = "/img/linocutboy_03_blue_turkeys.jpeg"
+	@art_description_03 = "Linocut | 38x38 cm"
+
+	@art_title_04 = "Coyote"
+	@art_image_04 = "/img/linocutboy_04_coyote.jpeg"
+	@art_description_04 = "Linocut | 25x30 cm"
+
 	erb(:linocutboy)
 end
